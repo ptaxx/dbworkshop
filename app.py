5e5161b24cd610f63dc8f7c25ec107f09cca5219
Roman Dryndik <dryndikroman@gmail.com>
2024-08-31 12:09:10 +0200

    Added new DB models.

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
+from model import Employee, Salary
 from session import Session
-from model import Employee
 
 
 def main():
     session = Session()
 
-    query = session.query(Employee)
+    employee = session.query(Employee).get(1)
+    print(employee)
 
-    result = query.all()
-    print(result)
+    # salaries = session.query(Salary).filter(
+    #     Salary.employee_id == employee.id
+    # )
+    # for salary in salaries:
+    #     print(salary)
+
+    employee.salaries.append(
+        Salary(amount=50000, bonus=5000, comments="Annual bonus")
+    )
+    # employee.pay(
+    #     amount=10000,
+    #     bonus=1000,
+    #     comments="Monthly salary",
+    # )
+    session.commit()
+
+    for salary in employee.salaries:
+        print(salary)
 
 
 if __name__ == "__main__":
--- model.py
+++ model.py
@@ -1,5 +1,14 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import declarative_base
+import datetime
+
+from sqlalchemy import (
+    Column,
+    ForeignKey,
+    Integer,
+    String,
+    DECIMAL,
+    Date,
+)
+from sqlalchemy.orm import declarative_base, relationship
 
 
 Base = declarative_base()
@@ -12,8 +21,40 @@ class Employee(Base):
     first_name = Column(String(50), nullable=False)
     last_name = Column(String(50), nullable=False)
 
+    salaries = relationship("Salary")
+
+    def pay(self, amount, bonus, date=None, comments=None):
+        salary = Salary(amount=amount, bonus=bonus)
+
+        if date is not None:
+            salary.date = date
+
+        if comments is not None:
+            salary.comments = comments
+
+        self.salaries.append(salary)
+
     def __repr__(self):
         return f"Employee({self.id}, {self.first_name}, {self.last_name})"
+
+
+class Salary(Base):
+    __tablename__ = "salaries"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    amount = Column(DECIMAL, nullable=False)
+    bonus = Column(DECIMAL, nullable=False, default=0)
+    date = Column(
+        Date,
+        nullable=False,
+        default=datetime.date.today
+    )
+    comments = Column(String(150))
+
+    employee_id = Column(Integer, ForeignKey("employees.id"), nullable=False)
+
+    def __repr__(self):
+        return f"Salary({self.amount}, {self.date}, {self.employee_id})"
 
 
 class Department(Base):
