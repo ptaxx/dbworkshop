8dba6788797bbec43de1596d4dc063bf8bb9150a
Roman Dryndik <dryndikroman@gmail.com>
2024-09-07 09:56:36 +0200

    Update session.py

--- session.py
+++ session.py
@@ -6,13 +6,14 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv(".env")
 
-DATABASE = os.environ["MYSQL_DATABASE"]
-HOST = os.environ["MYSQL_HOST"]
-PORT = os.environ["MYSQL_PORT"]
-USER = os.environ["MYSQL_USER"]
-PASSWORD = os.environ["MYSQL_PASSWORD"]
+DB_NAME = os.environ["DB_NAME"]
+DB_HOST = os.environ["DB_HOST"]
+DB_PORT = os.environ["DB_PORT"]
+DB_USER = os.environ["DB_USER"]
+DB_PASSWORD = os.environ["DB_PASSWORD"]
+URI_SCHEMA = os.environ["URI_SCHEMA"]
 
-URI = f"mysql+mysqlconnector://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
+URI = f"{URI_SCHEMA}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(URI)
 Session = sessionmaker(bind=engine)
